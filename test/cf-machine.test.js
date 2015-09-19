63b00a6e3fb55ec4ffcddfd078a8f841c20bd3ab
John Leszczynski <jrleszczynski@gmail.com>
2015-09-18 21:14:50 -0400

    test optional params

--- test/cf-machine.test.js
+++ test/cf-machine.test.js
@@ -7,5 +7,22 @@ test('cfMachine', function (t) {
 
   t.true(cfMachine instanceof MusicMachine)
 
+  var defaultGuide = cfMachine.createGuide()
+  t.deepEqual(defaultGuide.choices(),
+   [ 'C' ])
+  defaultGuide.choose('C4')
+  t.deepEqual(defaultGuide.choices().sort(),
+   [ 'D4', 'E4', 'F4', 'G4', 'A4', 'C5',
+     'B3', 'A3', 'C3', 'E3', 'F3', 'G3' ].sort())
+
+  var cfMachine_range5 = createCFmachine(5)
+  var guide_range5 = cfMachine_range5.createGuide()
+  t.deepEqual(guide_range5.choices(),
+   [ 'C' ])
+  guide_range5.choose('C4')
+  t.deepEqual(guide_range5.choices().sort(),
+   [ 'D4', 'E4', 'F4', 'G4',
+     'B3', 'A3', 'F3', 'G3' ].sort())
+
   t.end()
 })
