0a2e3845b2752c2eed5e9a00f1f418631a42fc76
John Leszczynski <jrleszczynski@gmail.com>
2017-01-23 12:02:17 -0500

    add pretty print test

---
+++ test/cantus-firmus/pretty-print-cf.test.js
@@ -0,0 +1,20 @@
+var test = require('tape')
+  , CantusFirmus = require('../../lib/cantus-firmus/cantus-firmus.js')
+  , prettyPrint = require('../../lib/cantus-firmus/pretty-print-cf.js')
+
+test('createCFmachine', function (t) {
+  var cf = new CantusFirmus('D minor', 6, 13)
+  var notes = 'D4 E4 F4 C4 D4 F4 E4 G4 Bb3 C4 F4 E4 D4'
+  notes.split(' ').forEach(cf.addNote)
+  var visualization = [
+  'G4                              o                      ',
+  'F4          o           o                   o          ',
+  'E4      o                   o                   o      ',
+  'D4  o               o                               o  ',
+  'C4              o                       o              ',
+  'Bb3                                 o                  ',
+  '    D4  E4  F4  C4  D4  F4  E4  G4  Bb3 C4  F4  E4  D4 '
+  ]
+  t.equal(prettyPrint(cf), visualization.join('\n'))
+  t.end()
+})
